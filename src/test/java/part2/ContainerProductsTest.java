85a79eb3b7bf9e181fa5e9143d6fdf435152c2c3
Dmytro Taranov <Dmytro_Taranov@epam.com>
2019-04-26 18:06:23 +0300

    TESTIIIII

--- src/test/java/part2/ContainerProductsTest.java
+++ src/test/java/part2/ContainerProductsTest.java
@@ -2,75 +2,143 @@ package part2;
 
 import org.junit.Before;
 import org.junit.Test;
+import part1.Beer;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class ContainerProductsTest {
-    private ContainerProducts containerProducts;
-    private final String firstPosition = "First position";
-    private final String secondPosition = "Second position";
-    private final String thirdPosition = "Third position";
-    private final int firstIndexPosition = 1;
+    List<Beer> list;
+    Beer beer1;
+    Beer beer2;
 
     @Before
     public void init(){
-        containerProducts = new ContainerProducts();
-        containerProducts.add(firstPosition);
-        containerProducts.add(secondPosition);
-        containerProducts.add(thirdPosition);
-    }
-
-
-    @Test
-    public void isAddInputElementIntoCollection(){
-        String expectedValue = secondPosition;
-        assertEquals(expectedValue, containerProducts.toArray()[firstIndexPosition]);
+        list = new ContainerProducts<>();
+        beer1 = new Beer();
+        beer2 = new Beer();
     }
 
     @Test
-    public void isGetGiveElementFromIndex(){
-        String expectedValue = secondPosition;
-        Object actual = containerProducts.get(firstIndexPosition);
-        assertEquals(expectedValue, actual);
+    public void sizeShouldReturnZeroWhenCallThisMethod(){
+        int actually = 0;
+        int expected = list.size();
+        assertEquals(actually, expected);
+    }
+
+    //Добавить проверсу на ClassCastException
+    //Добавить проверку на NullPointerException
+    @Test
+    public void containsShouldReturnTrueIfThisListContainsElement(){
+        Beer actually = beer1;
+        list.add(beer1);
+        assertTrue(list.contains(actually));
     }
 
     @Test
-    public void isRemoveToIndexDeleteElement(){
+    public void toArrayShouldReturnAnArrayOfThisList(){
+        Beer[] b = {beer1, beer2};
+        list.add(beer1);
+        list.add(beer2);
+        assertArrayEquals(b, list.toArray());
+    }
+
+  /*  @Test
+    public void toArrayWithParamsShouldReturnAnArrayOfThisList(){
+        [] b = {1, 2, 3} ;
+        list.add(beer1);
+        list.add(beer2);
+        assertArrayEquals(b, list.toArray(b));
+    } */
+
+    //Добавить проверсу на ClassCastException
+    //Добавить проверку на NullPointerException
+    @Test
+    public void addShouldIncrementSizeWhenInsertElement(){
+        int i = list.size();
+        list.add(beer1);
+        assertEquals(i+1,list.size());
+    }
+
+    @Test
+    public void addShouldAddAppendElementIntoEndOfTheList(){
+        list.add(beer1);
+        assertEquals(list.toArray()[list.size()-1], beer1);
+    }
+
+
+    //Добавить проверсу на ClassCastException
+    //Добавить проверку на NullPointerException
+    @Test
+    public void removeByObjectShouldDecrementSizeWhenDeleteElement(){
+        int i = list.size();
+        list.add(beer1);
+        list.remove(beer1);
+        assertEquals(list.size(), i);
+    }
+
+    @Test
+    public void removeByObjectShouldDeleteElementFromList(){
+        list.add(beer1);
+        list.remove(beer1);
+        assertTrue(list.size() < 1);
+    }
+
+    @Test
+    public void removeMustReturnFalseIfElementIsNotExist(){
+        boolean flag = list.remove(beer1);
+        assertFalse(flag);
+    }
+
+    //Добавить тест на ContainsAll
+
+    @Test
+    public void addAllShouldIncreaseSizeOfCustomCollectionByTheNumberOfItemsInTheCollectionInParam(){
         ArrayList test = new ArrayList();
-        test.add(firstPosition);
-        test.add(thirdPosition);
-        containerProducts.remove(firstIndexPosition);
-        assertEquals(Arrays.toString(containerProducts.toArray()), test.toString());
-
+        test.add(beer1);
+        test.add(beer2);
+        list.add(beer1);
+        int addedCollectionSize = test.size();
+        int expectedCollectionSize = list.size() + addedCollectionSize;
+        list.addAll(test);
+        assertEquals(list.size(), expectedCollectionSize);
     }
 
+    //Добавить проверсу на ClassCastException
+    //Добавить проверку на NullPointerException
     @Test
-    public void isRemoveToObjectDeleteElement(){
+    public void addAllShouldInputAllElementsFromCollectionInParamInTheEndOfList(){
+        list.add(beer1);
         ArrayList test = new ArrayList();
-        test.add(firstPosition);
-        test.add(thirdPosition);
-        containerProducts.remove(secondPosition);
-        assertEquals(Arrays.toString(containerProducts.toArray()), test.toString());
+        test.add(beer1);
+        test.add(beer2);
+        list.addAll(test);
+        int count = 0;
+        for (int i = 1; i < list.size(); i++) {
+            if (list.toArray()[i].equals(test.toArray()[i-1])){
+                count++;
+            }
+        }
+        assertEquals(count, 2);
     }
 
-    @Test
-    public void isAddByIndexInputElementIntoCollection(){
-        containerProducts.add(firstIndexPosition, thirdPosition);
-        assertEquals(containerProducts.toArray()[firstIndexPosition], thirdPosition);
-    }
 
-    @Test
-    public void isIteratorWork(){
-        Iterator iterator = containerProducts.iterator();
-        iterator.hasNext();
-        iterator.next();
-        Object o = iterator.next();
-        assertEquals(o, containerProducts.toArray()[firstIndexPosition]);
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
